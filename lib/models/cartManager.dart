457e4e5ba6e13131a2b19b9365a143268283fa83
Troub <matveyv2004@gmail.com>
2024-12-25 19:50:36 +0300

    -

--- lib/models/cartManager.dart
+++ lib/models/cartManager.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:pr3/models/product.dart';
+import 'package:pr3/api/api.dart';
 
-class CartManager with ChangeNotifier{
+class CartManager with ChangeNotifier {
+  final ApiService _apiService = ApiService();
   final List<Product> _cartProducts = <Product>[];
 
   List<Product> get cartProducts => _cartProducts;
 
-  void addToCart(Product product){
-    if (!_cartProducts.contains(product)){
+  Future<void> addToCart(Product product) async {
+    if (!_cartProducts.contains(product)) {
       _cartProducts.add(product);
       product.quantity++;
+      product.inCart = true;
+      await _apiService.updateProductQuantity(
+          product.productId, product.quantity);
+      await _apiService.toggleCart(product.productId);
       notifyListeners();
     }
   }
 
-  void removeFromCart(Product product, BuildContext context){
+  Future<void> removeFromCart(Product product, BuildContext context) async {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -24,19 +30,23 @@ class CartManager with ChangeNotifier{
           content: const Text('Вы уверены, что хотите удалить этот товар?'),
           actions: <Widget>[
             TextButton(
-              child: const Text('Отмена',
-                style: TextStyle(color: Colors.black),
-              ),
+              child: const Text(
+                  'Отмена', style: TextStyle(color: Colors.black)),
               onPressed: () {
                 Navigator.of(context).pop();
               },
             ),
             TextButton(
-              child: const Text('Удалить',
-                style: TextStyle(color: Colors.black),
-              ),
-              onPressed: () {
+              child: const Text(
+                  'Удалить', style: TextStyle(color: Colors.black)),
+              onPressed: () async {
                 _cartProducts.remove(product);
+                product.quantity = 0;
+                product.inCart = false; // Сбрасываем флаг InCart
+                await _apiService.updateProductQuantity(
+                    product.productId, product.quantity);
+                await _apiService.toggleCart(
+                    product.productId); // Обновляем флаг InCart на сервере
                 notifyListeners();
                 Navigator.of(context).pop();
               },
@@ -45,20 +55,22 @@ class CartManager with ChangeNotifier{
         );
       },
     );
-    product.quantity = 0;
-  }
 
-  bool isInCart(Product product){
-    return _cartProducts.contains(product);
+
+    bool isInCart(Product product) {
+      return _cartProducts.contains(product);
+    }
   }
 
   double getTotalPrice() {
-    return _cartProducts.fold(0,(sum, product) {
+    return _cartProducts.fold(0, (sum, product) {
       return sum + (product.productPrice * product.quantity);
     });
   }
+
   void updateQuantity(Product product, int newQuantity) {
-    final index = _cartProducts.indexWhere((p) => p.productId == product.productId);
+    final index = _cartProducts.indexWhere((p) =>
+    p.productId == product.productId);
     if (index != -1) {
       _cartProducts[index].quantity = newQuantity;
       notifyListeners();
